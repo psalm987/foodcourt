e509bb50c19fd0136da2fd9e260043d478e2e645
Samuel Seibidor <samrythmn@gmail.com>
2023-02-18 06:23:00 +0100

    Added and configured Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { GlobalStyles } from "@mui/material";
+import React from "react";
 import Login from "./pages/login";
+import Profile from "./pages/profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Login />;
+  return (
+    <React.Fragment>
+      <GlobalStyles
+        styles={{
+          "*": {
+            boxSizing: "border-box",
+            fontFamily: "Manrope, sans-serif",
+          },
+          "#root, body": {
+            padding: 0,
+            margin: 0,
+          },
+        }}
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
